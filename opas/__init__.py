4d766ec3d9925e2b04ee71d91c6a10b541b7c5ab
wiethkaty <tomorrow.is.another.day1829@gmail.com>
2020-11-25 23:35:45 +0900

    Revert "ベスプラURL"
    This reverts commit fc7fac89ee337b21e95c78e6fe8c2d70a769fea2.

--- opas/__init__.py
+++ opas/__init__.py
@@ -5,5 +5,3 @@ from opas.gym import *
 
 # Check below:
 #   https://qiita.com/FN_Programming/items/2dcabc93365a62397afe
-# Best Practice:
-#   https://rinatz.github.io/python-book/ch04-07-project-structures/
