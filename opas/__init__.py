2b73482f4fb37d33205f5ccc05c9a2be6085d907
wiethkaty <tomorrow.is.another.day1829@gmail.com>
2020-11-25 23:35:54 +0900

    Revert "ディレクトリ構成をベスプラ準拠"
    This reverts commit 95171f72fc6327d124aa222a2aaf63085148702d.

--- opas/__init__.py
+++
@@ -1,7 +0,0 @@
-from opas.__main__ import *
-from opas.common import *
-from opas.line_notify_bot import *
-from opas.gym import *
-
-# Check below:
-#   https://qiita.com/FN_Programming/items/2dcabc93365a62397afe
