95171f72fc6327d124aa222a2aaf63085148702d
yoshizaki.kota <yoshizaki.kota@cloud-ace.jp>
2020-11-25 18:06:19 +0900

    ディレクトリ構成をベスプラ準拠

---
+++ opas/__init__.py
@@ -0,0 +1,7 @@
+from opas.__main__ import *
+from opas.common import *
+from opas.line_notify_bot import *
+from opas.gym import *
+
+# Check below:
+#   https://qiita.com/FN_Programming/items/2dcabc93365a62397afe
